c81b44a1d119c3dd034c72c44a79ae3dfc58e962
JojoDuke <jojoamankwa@gmail.com>
2025-03-26 11:55:56 +0000

    rewire

--- backend/chatCollection.py
+++ backend/chatCollection.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.wetrocloud.com/v1/collection/query/"
+url = "https://api.wetrocloud.com/v1/collection/chat/"
 headers = {
         "Authorization": "Token wtc-sk-ff86dcf1bdf90981a679044600396edfab43bf01"
     }
@@ -8,9 +8,10 @@ headers = {
 data = {
     "collection_id": "test4",
     "request_query": "What was the price",
+    "history": "",
     "model": "gpt-4o"
 }
 
 
-response = requests.post(url, data=data, headers=headers)
+response = requests.post(url, json=data, headers=headers)
 print(response.text)
--- backend/createCollection.py
+++ backend/createCollection.py
@@ -1,16 +1,17 @@
 import requests
 
-url = "https://api.wetrocloud.com/v1/collection/query/"
+url = "https://api.wetrocloud.com/v1/resource/insert/"
 headers = {
         "Authorization": "Token wtc-sk-ff86dcf1bdf90981a679044600396edfab43bf01"
     }
 
 data = {
     "collection_id": "test4",
-    "request_query": "What was the price",
-    "model": "gpt-4o"
+    "resource": "https://res.cloudinary.com/dtolochpp/image/upload/v1742988113/namecheap-order-97954665_psrpri.pdf",
+    "type":"file"
 }
 
 
 response = requests.post(url, data=data, headers=headers)
 print(response.text)
+
