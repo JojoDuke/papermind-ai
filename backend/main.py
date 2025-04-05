24f29f43abbd0401794495d6fcb6c3eb40819df1
JojoDuke <jojoamankwa@gmail.com>
2025-04-05 20:16:51 +0000

    jkj

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","http://usepapermind.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
